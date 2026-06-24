42ec7d53ec70bb009c162e1e0efb18104ed0946a
zhangxiaoyong <xiaoyong.zhang@3zyper.com>
2026-06-24 22:50:09 +0800

    chore: scaffold kb-platform project

---
+++ kb_platform/__init__.py
@@ -0,0 +1,3 @@
+"""Knowledge base management platform on top of GraphRAG."""
+
+__version__ = "0.1.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,5 @@
+import kb_platform
+
+
+def test_package_imports():
+    assert kb_platform.__version__ == "0.1.0"
